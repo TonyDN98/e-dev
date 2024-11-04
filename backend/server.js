ef6414d27588fe6f8b9c0ffe24f578ab46b56227
TonyDN98 <107117383+TonyDN98@users.noreply.github.com>
2024-11-04 11:52:15 +0200

    part 2

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 // Middleware
 app.use(express.json());
@@ -10,5 +13,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Serverul funcționează!');
 });
+
+// Conectare la MongoDB
+mongoose.connect(process.env.MONGODB_URI).then(() => console.log('Conectat la MongoDB')) .catch((error) => console.error('Eroare la conectarea MongoDB:', error));
 
 app.listen(PORT, () => console.log(`Server pornit pe portul ${PORT}`));
